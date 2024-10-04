dc7c3662eaa7ea814687bf23f19e34ede525a873
Feysal254 <feisalmanyara@icloud.com>
2024-10-04 11:02:36 -0700

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const arr = [19, 20, 34];
+  const newArr = [];
+  for (const i of arr) {
+    newArr.push(new ClassRoom(i));
+  }
+  return newArr;
+}
